94026fead21f20d16f3dc2c253bab9be23ef1613
konstantin <sit.com.k@gmail.com>
2021-04-12 02:44:30 +0300

    4

--- src/Cli.php
+++ src/Cli.php
@@ -1,12 +1,17 @@
 <?php
+
 namespace BrainGames\src;
 
   use function cli\line;
   use function cli\prompt;
-
-  function hello()
-  {
-  	    line('Welcome to the Brain Game!');
-  		$name = prompt('May I have your name?');
-  		line("Hello, %s!", $name);
-  }
+/**
+ * Make a greating
+ * 
+ * @return greeentin
+ */
+function hello()
+{
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+}
