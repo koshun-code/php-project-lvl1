2594a9b0f10c8f33574fedde7534e4b69efb1722
konstantin <sit.com.k@gmail.com>
2021-04-14 14:38:29 +0300

    finish step 5

--- src/Cli.php
+++ src/Cli.php
@@ -14,25 +14,31 @@ function hello()
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
+    return $name;
 }
 /**
- * is Even or not
- * @param  [integer] $number [number]
+ * Is Even or not
+ * 
+ * @param [integer] $number [number]
+ * 
  * @return [bool]         [true or false]
  */
 function isEven($number)
 {
-	return $number % 2 === 0 ? true : false;
+    return $number % 2 === 0 ? true : false;
 }
 /**
  * [wrongAnswer description]
- * @param  [string] $name          [description]
- * @param  [string] $userAnswer    [description]
- * @param  [string] $correctAnswer [description]
+ * 
+ * @param [string] $name          [description]
+ * 
+ * @param [string] $userAnswer    [description]
+ * 
+ * @param [string] $correctAnswer [description]
+ * 
  * @return [string]                [description]
  */
 function wrongAnswer($name, $userAnswer, $correctAnswer)
 {
-    sprintf("'%s' is wrong answer ;(. Correct answer was '%s'.
-    Let's try again, %s!", $userAnswer, $correctAnswer, $name);
+    print_r("'{$userAnswer}' is wrong answer ;(. Correct answer was '{$correctAnswer}'.Let's try again, {$name}! \n");
 }
