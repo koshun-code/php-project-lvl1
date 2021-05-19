f9d39405110cdb0da951b4449c4e859660670a4a
Konstantin <sit.com.k@gmail.com>
2021-05-19 16:29:27 +0300

    sdf

--- src/Games/Progression.php
+++ src/Games/Progression.php
@@ -2,12 +2,22 @@
 
 namespace BrainGames\Games\Progression;
 
-use function BrainGames\Cli\setProgression;
-use function BrainGames\Cli\getRandNum;
+//use function BrainGames\Cli\setProgression;
+//use function BrainGames\Cli\getRandNum;
 use function BrainGames\Engine\engine;
 
 const DESCRIPTION = "What number is missing in the progression?";
+define('LENGTH', 10);
 
+function generateProgression($startProgression, $diffProgression, $length)
+{
+    $progression = [];
+    for ($i = 0; $i < $length; $i++) {
+        $progression[$i] = $startProgression + $diffProgression * $i;
+    }
+    return $progression;
+}
+/*
 function getData()
 {
     $result = [];
@@ -20,7 +30,22 @@ function getData()
     }
     return $result;
 }
-
+*/
+function getData()
+{
+    $questionsAnsewrs = [];
+    for ($i = 1; $i < 3; $i++) {
+        $startProgression = mt_rand(1, 20);
+        $diffProgression = mt_rand(1, 10);
+        $progression = generateProgression($startProgression, $diffProgression, LENGTH);
+        $unknownNumPosition = mt_rand(0, LENGTH - 1);
+        $answer = $progression[$unknownNumPosition];
+        $progression[$unknownNumPosition] = "..";
+        $question = implode(" ", $progression);
+        $questionsAnsewrs[] = [$question,$answer];
+    }
+    return $questionsAnsewrs;
+}
 function brainProgression()
 {
     $data = getData();
